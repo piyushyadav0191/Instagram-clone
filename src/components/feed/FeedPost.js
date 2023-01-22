ba4fc6cf67c33709d99a69d54534089041d9f954
piyush yadav <piyushyadav0191@gmail.com>
2023-01-22 11:36:15 +0530

    feed-ui-with-static-data

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -1,10 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFeedPostStyles } from "../../styles";
+import UserCard from "../shared/UserCard";
+import { CommentIcon, MoreIcon, ShareIcon } from "../../icons";
+import { Link } from "react-router-dom";
+import { Button, Divider, Hidden, Typography } from "@material-ui/core";
+import HTMLEllipsis from "react-lines-ellipsis/lib/html";
 
-function FeedPost() {
-  useFeedPostStyles();
+function FeedPost({ post }) {
+  const classes = useFeedPostStyles();
+  const [showCaption, setCaption] = useState(false);
+  const { id, media, likes, user, caption, comments } = post;
 
-  return <div>FeedPost</div>;
+  return (
+    <article className={classes.article}>
+      {/* feed post header */}
+      <div className={classes.postHeader}>
+        <UserCard user={user} />
+        <MoreIcon className={classes.moreIcon} />
+      </div>
+      <div>
+        <img src={media} alt="Post media" className={classes.image} />
+      </div>
+      <div className={classes.postButtonsWrapper}>
+        <div className={classes.postButtonsWrapper}>
+          <LikeButton />
+          <Link to={`/p/${id}`}>
+            <CommentIcon />
+          </Link>
+          <ShareIcon />
+          <SaveButton />
+        </div>
+        <Typography className={classes.like} variant="subtitle2">
+          <span>{likes === 1 ? "1 Like" : `${likes} Likes`} </span>
+        </Typography>
+        <div className={showCaption ? classes.expanded : classes.collapsed}>
+          <Link to={`/${user.username}`}>
+            <Typography
+              variant="subtitle2"
+              component="span"
+              className={classes.username}
+            >
+              {user.username}
+            </Typography>
+          </Link>
+          {showCaption ? (
+            <Typography
+              varant="body2"
+              component="span"
+              dangerouslySetInnerHTML={{ __html: caption }}
+            />
+          ) : (
+            <div className={classes.captionWrapper}>
+              <HTMLEllipsis
+                unsafeHTML={caption}
+                className={classes.caption}
+                maxLine="0"
+                ellipsis="..."
+                basedOn="letters"
+              />
+              <Button
+                className={classes.moreButton}
+                onClick={() => setCaption(true)}
+              >
+                more
+              </Button>
+            </div>
+          )}
+        </div>
+        <Link to={`/p/${id}`}>
+          <Typography
+            className={classes.commentsLink}
+            variant="body2"
+            component="div"
+          >
+            View all {comments.length} comments
+          </Typography>
+        </Link>
+        {comments.map((comment) => (
+          <div key={comment.id}>
+            <Link to={`/${comment.user.username}`}>
+              <Typography
+                variant="subtitle2"
+                component="span"
+                className={classes.commentUsername}
+              >
+                {comment.user.username}
+              </Typography>{" "}
+              <Typography variant="body2" component="span">
+                {comment.contet}
+              </Typography>
+            </Link>
+          </div>
+        ))}
+        <Typography color="textSecondary" className={classes.datePosted}>
+          5 Days Ago
+        </Typography>
+      </div>
+      <Hidden xsDown>
+        <Divider />
+        <Comment />
+      </Hidden>
+    </article>
+  );
+}
+
+function LikeButton() {
+  return <>LikeButton</>;
+}
+function SaveButton() {
+  return <>SaveButton</>;
+}
+function Comment() {
+  return <>Comment</>;
 }
 
 export default FeedPost;
--- src/components/shared/UserCard.js
+++ src/components/shared/UserCard.js
@@ -1,10 +1,33 @@
 import React from "react";
 import { useUserCardStyles } from "../../styles";
+import { Link } from "react-router-dom";
+import { Avatar, Typography } from "@material-ui/core";
 
-function UserCard() {
-  useUserCardStyles();
+function UserCard({ user }) {
+  const classes = useUserCardStyles();
+  const { username, profile_image, name } = user;
 
-  return <div>UserCard</div>;
+  return (
+    <div className={classes.wrapper}>
+      <Link to={`/${username}`}>
+        <Avatar src={profile_image} alt="avatar" className={classes.avatar} />
+      </Link>
+      <div className={classes.nameWrapper}>
+        <Link to={`/${username}`}>
+          <Typography variant="subtitle2" className={classes.typography}>
+            {username}
+          </Typography>
+        </Link>
+        <Typography
+          color="textSecondary"
+          variant="body2"
+          className={classes.typography}
+        >
+          {name}
+        </Typography>
+      </div>
+    </div>
+  );
 }
 
 export default UserCard;
